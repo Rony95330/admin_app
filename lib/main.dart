f44b0518b2e777fb88b905f838da8f098a70f660
Rony95330 <32642351+Rony95330@users.noreply.github.com>
2025-09-26 16:25:22 +0200

    Mise à jour : notifications plein écran + manifest

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 import 'pages/dashboard_page.dart';
 import 'pages/database_page.dart';
 import 'pages/notifications_page.dart';
 import 'widgets/side_menu.dart';
+
+// 🔔 Plugin global pour les notifs locales
+final FlutterLocalNotificationsPlugin localNotif =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> initLocalNotifications() async {
+  const AndroidInitializationSettings initAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  const InitializationSettings settings =
+      InitializationSettings(android: initAndroid);
+
+  await localNotif.initialize(settings);
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // ⚡️ Initialisation Supabase
   await Supabase.initialize(
-    url:
-        'https://qfvogtbdqotbvmpxalwx.supabase.co', // ← remplace par ton URL Supabase
+    url: 'https://qfvogtbdqotbvmpxalwx.supabase.co',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmdm9ndGJkcW90YnZtcHhhbHd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjgyNzgsImV4cCI6MjA3MDE0NDI3OH0.noHGv6Oa70cKfKIefPqU_feufiYB5JOwyWLo2H4Pp3A', // ← remplace par ta clé anonyme
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmdm9ndGJkcW90YnZtcHhhbHd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjgyNzgsImV4cCI6MjA3MDE0NDI3OH0.noHGv6Oa70cKfKIefPqU_feufiYB5JOwyWLo2H4Pp3A',
   );
+
+  // 🔔 Initialisation des notifs locales
+  await initLocalNotifications();
 
   runApp(const AdminApp());
 }
